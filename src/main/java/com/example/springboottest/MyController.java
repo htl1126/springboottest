ae6ddf8425482b2bdb89eca4792991503199f35c
Tian-Liang Huang <tianliang.huang@welhunt.com>
2024-08-16 15:09:39 +0800

    added a POST handler

--- src/main/java/com/example/springboottest/MyController.java
+++ src/main/java/com/example/springboottest/MyController.java
@@ -1,7 +1,11 @@
 package com.example.springboottest;
 
+import java.util.Map;
+import java.util.HashMap;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -10,6 +14,16 @@ public class MyController {
     @GetMapping("/healthcheck")
     public String healthCheck() {
         return "This service is running healthy";
+    }
+
+    @PostMapping("/getName")
+    public Map<String, String> getName(@RequestBody Username user) {
+        Map<String, String> name = new HashMap<>();
+
+        name.put("first", user.first);
+        name.put("last", user.last);
+
+        return name;
     }
 
 }
---
+++ src/main/java/com/example/springboottest/Username.java
@@ -0,0 +1,5 @@
+package com.example.springboottest;
+
+public class Username {
+    public String first, last;
+}
