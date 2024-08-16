7134f3f9ce2cb901b42d516ea51bb556498b5f71
Tian-Liang Huang <tianliang.huang@welhunt.com>
2024-08-16 16:28:46 +0800

    added a filter

---
+++ src/main/java/com/example/springboottest/SimpleFilter.java
@@ -0,0 +1,21 @@
+package com.example.springboottest;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+@Order(0)
+public class SimpleFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
+        System.out.println("At SimpleFilter");
+    }
+
+}
